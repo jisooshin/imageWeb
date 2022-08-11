c5f595b5a6e908ae44a7de9f5bcae41d9b098a94
shinjisoo <jisoo.shin.87@gmail.com>
2022-08-11 21:17:36 +0900

    등록된 캔버스의 array 뽑기

--- static/js/base.js
+++ static/js/base.js
@@ -1,21 +1,26 @@
-
+var data;
 function renderImage(src, canv) {
 	let presetImage = new Image();
 	presetImage.src = src;
+	if (!src.includes("image"))
+	{
+		window.alert("You have to upload image file.");
+		throw "[ERROR] It can take image file only.";
+	}
+
 	let ctx = canv.getContext("2d");
+	let imgWidth, imgHeight;
+
 	presetImage.addEventListener("load", function(){
-		let imgWidth = this.naturalWidth;
-		let imgHeight = this.naturalHeight;
+		imgWidth = this.naturalWidth;
+		imgHeight = this.naturalHeight;
 		canv.width = imgWidth;
 		canv.height = imgHeight;
 		ctx.drawImage(this, 0, 0);
+		data = ctx.getImageData(0, 0, imgWidth, imgHeight).data;
 	});
 }
 
-window.onload = function(){
-	let canv = document.querySelector("canvas#image");
-	renderImage("image/hamster.jpg", canv)
-}
 function uploadImage(){
 	let canv = document.querySelector("canvas#image");
 	let elem = document.querySelector("input#file-upload");
@@ -25,10 +30,14 @@ function uploadImage(){
 	reader.addEventListener('load', () => {
 		renderImage(reader.result, canv);
 	});
-
-
 	console.log("File uploaded.");
+}
 
+
+
+window.onload = function(){
+	let canv = document.querySelector("canvas#image");
+	renderImage("image/hamster.jpg", canv)
 }
 
 
